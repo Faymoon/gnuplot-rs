8da721c0c6471f1f7bcf23ed31650ea1fd348e7c
Faymoon <mercier.mateo@orange.fr>
2020-01-08 14:34:33 +0100

    make this example more complete

--- examples/multigraph.rs
+++ examples/multigraph.rs
@@ -13,7 +13,7 @@ fn main()
 
     canvas.set_coordinate_system(CoordinateSystem::Cartesian); // this requires that expressions for graphs will be in function of x
 
-    let graph = Graph::new(GraphStyles::Lines2d(vec![[0.0, 0.0], [5.0, 5.0], [8.0, -1.0]]));
+    let graph = Graph::new(GraphStyles::Lines2d(vec![[0.0, 0.0], [5.0, 5.0], [8.0, -1.0]])).axes(XAxes2d::X1, YAxes2d::Y2);
     let sinus = Graph::new(GraphStyles::Lines2dExpr("sin(x)"));
     let test = Graph::new(GraphStyles::Lines2dExpr("cos(x)"));
 
