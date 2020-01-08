ac305c4a1aefd43006a0359b0360c149a5a3f221
Faymoon <mercier.mateo@orange.fr>
2020-01-08 14:35:50 +0100

    little code refactor

--- src/graph.rs
+++ src/graph.rs
@@ -47,8 +47,8 @@ pub enum GraphStyles // to understand how value should be used check that http:/
 	BoxPlot(Vec<[f64; 4]>),
 	Ellipses(Vec<[f64; 5]>),
 	HistogramsClustered(Vec<f64>, f64),             //    /\
-	HistogramsRowStacked(Vec<f64>),                //   /  \
-	HistogramsColumnStacked(Vec<f64>),             //  /warn\ : different histogram types can not be displayed together
+	HistogramsRowStacked(Vec<f64>),                 //   /  \
+	HistogramsColumnStacked(Vec<f64>),              //  /warn\ : different histogram types can not be displayed together
 	HistogramsErrorBars(Vec<[f64; 3]>, f64),        // /______\
 	RGBAlpha,
 	BoxErrorBars(Vec<[f64; 5]>),
@@ -156,48 +156,77 @@ impl Graph
 	{
 		canvas.on_graph_add();
 
-		let mut plot_arg = String::new();
+		let mut plot_arg_style = String::new();
+		let mut plot_arg_data = String::new();
 
 		match &self.data
 		{
 			GraphStyles::Lines2d(data) =>
 				{
-					plot_arg.push_str("'-' with lines");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("lines");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
-			GraphStyles::Lines2dExpr(string) => plot_arg.push_str(string),
+			GraphStyles::Lines2dExpr(string) => 
+				{
+					plot_arg_data.push_str(&string.replacen(char::is_whitespace, "", string.len()));
+					plot_arg_style.push_str("lines");
+				},
 			GraphStyles::Dots2d(data) =>
 				{
-					plot_arg.push_str("'-' with dots");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("dots");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
-			GraphStyles::Dots2dExpr(string) => plot_arg.push_str(&format!("{} with dots", string)),
+			GraphStyles::Dots2dExpr(string) => 
+				{
+					plot_arg_data.push_str(&string.replacen(char::is_whitespace, "", string.len()));
+					plot_arg_style.push_str("dots");
+				},
 			GraphStyles::Points2d(data) =>
 				{
-					plot_arg.push_str("'-' with points");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("points");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
-			GraphStyles::Points2dExpr(string) => plot_arg.push_str(&format!("{} with points", string)),
+			GraphStyles::Points2dExpr(string) => 
+				{
+					plot_arg_data.push_str(&string.replacen(char::is_whitespace, "", string.len()));
+					plot_arg_style.push_str("points");
+				},
 			GraphStyles::LinesPoints2d(data) =>
 				{
-					plot_arg.push_str("'-' with linespoints");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("linespoints");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
-			GraphStyles::LinesPoints2dExpr(string) => plot_arg.push_str(&format!("{} with linespoints", string)),
+			GraphStyles::LinesPoints2dExpr(string) => 
+				{
+					plot_arg_data.push_str(&string.replacen(char::is_whitespace, "", string.len()));
+					plot_arg_style.push_str("linespoints");
+				},
 			GraphStyles::Impulses2d(data) =>
 				{
-					plot_arg.push_str("'-' with impulses");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("impulses");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
-			GraphStyles::Impulses2dExpr(string) => plot_arg.push_str(&format!("{} with impulses", string)),
+			GraphStyles::Impulses2dExpr(string) => 
+				{
+					plot_arg_data.push_str(&string.replacen(char::is_whitespace, "", string.len()));
+					plot_arg_style.push_str("impulses");
+				},
 			GraphStyles::Vectors2d(data) =>
 				{
-					plot_arg.push_str("'-' with vectors");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("vectors");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::Labels2d(data) =>
 				{
-					plot_arg.push_str("'-' with labels");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("labels");
+
 					let mut plot_data = String::new();
 					for d in data.iter()
 					{
@@ -213,53 +242,73 @@ impl Graph
 				},
 			GraphStyles::Steps(data) =>
 				{
-					plot_arg.push_str("'-' with steps");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("steps");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
-			GraphStyles::StepsExpr(string) => plot_arg.push_str(&format!("{} with steps", string)),
+			GraphStyles::StepsExpr(string) => 
+				{
+					plot_arg_data.push_str(&string.replacen(char::is_whitespace, "", string.len()));
+					plot_arg_style.push_str("steps");
+				},
 			GraphStyles::FSteps(data) =>
 				{
-					plot_arg.push_str("'-' with fsteps");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("fsteps");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
-			GraphStyles::FStepsExpr(string) => plot_arg.push_str(&format!("{} with fsteps", string)),
+			GraphStyles::FStepsExpr(string) => 
+				{
+					plot_arg_data.push_str(&string.replacen(char::is_whitespace, "", string.len()));
+					plot_arg_style.push_str("fsteps");
+				},
 			GraphStyles::YErrorBars(data) =>
 				{
-					plot_arg.push_str("'-' with yerrorbars");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("yerrorbars");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::XErrorBars(data) =>
 				{
-					plot_arg.push_str("'-' with xerrorbars");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("xerrorbars");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::XYErrorBars(data) =>
 				{
-					plot_arg.push_str("'-' with xyerrorbars");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("xyerrorbars");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::YErrorLines(data) =>
 				{
-					plot_arg.push_str("'-' with yerrorlines");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("yerrorlines");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::XErrorLines(data) =>
 				{
-					plot_arg.push_str("'-' with xerrorlines");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("xerrorlines");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::XYErrorLines(data) =>
 				{
-					plot_arg.push_str("'-' with xyerrorlines");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("xyerrorlines");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::FinanceBars(data) =>
 				{
-					plot_arg.push_str("'-' with financebars");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("financebars");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::ParallelAxes(data, width) =>
 				{
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("parallelaxes");
+					
 					let mut plot_data = String::new();
 
 					let mut i: usize = 1;
@@ -280,38 +329,47 @@ impl Graph
 				},
 			GraphStyles::Boxes(data) =>
 				{
-					plot_arg.push_str("'-' with boxes");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("boxes");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::BoxPlot(data) =>
 				{
-					plot_arg.push_str("'-' with boxplot");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("boxplot");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::BoxXYError(data) =>
 				{
-					plot_arg.push_str("'-' with boxxyerror");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("boxxyerror");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::Candlesticks(data) =>
 				{
-					plot_arg.push_str("'-' with candlesticks");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("candlesticks");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::Circles(data) =>
 				{
-					plot_arg.push_str("'-' with circles");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("circles");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::Ellipses(data) =>
 				{
-					plot_arg.push_str("'-' with ellipses");
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("ellipses");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::HistogramsClustered(data, gap) =>
 				{
 					canvas.add_pre_plot_command(&format!("set style histogram clustered gap {}", gap));
-					plot_arg.push_str("'-' with histograms");
+					
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("histograms");
+
 					let mut plot_data = String::new();
 					for val in data.iter()
 					{
@@ -323,13 +381,19 @@ impl Graph
 			GraphStyles::HistogramsErrorBars(data, gap) =>
 				{
 					canvas.add_pre_plot_command(&format!("set style histogram errorbars gap {}", gap));
-					plot_arg.push_str("'-' with histograms");
+					
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("histograms");
+					
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			GraphStyles::HistogramsColumnStacked(data) =>
 				{
 					canvas.add_pre_plot_command("set style histogram columnstacked");
-					plot_arg.push_str("'-' with histograms");
+					
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("histograms");
+					
 					let mut plot_data = String::new();
 					for val in data.iter()
 					{
@@ -341,7 +405,10 @@ impl Graph
 			GraphStyles::HistogramsRowStacked(data) =>
 				{
 					canvas.add_pre_plot_command("set style histogram rowstacked");
-					plot_arg.push_str("'-' with histograms");
+					
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("histograms");
+					
 					let mut plot_data = String::new();
 					for val in data.iter()
 					{
@@ -351,14 +418,15 @@ impl Graph
 					canvas.add_plot_data(&plot_data);
 				},
 			GraphStyles::BoxErrorBars(data) =>
-				{
-					plot_arg.push_str("'-' with boxerrorbars");
+				{		
+					plot_arg_data.push_str("'-'");
+					plot_arg_style.push_str("boxerrorbars");
 					canvas.add_plot_data(&parse_plot_data(data.iter().map(|x| x as &[f64]).collect()));
 				},
 			_ => unimplemented!(),
 		}
-		
-		plot_arg = plot_arg.replace(" with", &format!(" axes {}{} with", self.axes.0.to_string(), self.axes.1.to_string()));
+
+		let mut plot_arg = format!("{} axes {}{} with {}", plot_arg_data, self.axes.0.to_string(), self.axes.1.to_string(), plot_arg_style);
 
 		if !self.title.is_empty()
 		{
